b75e767fe1999a8266c0ed4c285fd3db5121df6c
khaleelrahman <khaleelrahman760@gmail.com>
2022-01-26 16:45:03 +0530

    out of stock item added

--- src/component/collectionItem/collectionItem.jsx
+++ src/component/collectionItem/collectionItem.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './collectionItem.scss'
 import CustomButton from '../custombutton/custombutton';
 import { connect } from 'react-redux';
+import Outof  from "../../assets/outof.jpg";
 
 const CollectionItem=({item,AddItem})=>{
-    const {id,name,price,imageUrl,vendor}=item
+    const {id,name,price,imageUrl,vendor,available}=item
     return(
         <div className='collection-item'>
             <div 
@@ -14,6 +15,7 @@ const CollectionItem=({item,AddItem})=>{
                 }}
             
             />
+            {!available&&<img className='stock-out' src={Outof}/>}
         <div className='collection-footer'>
             <span className='name'>{name}({vendor})</span>
             <span className='price'>₹{price}</span>
